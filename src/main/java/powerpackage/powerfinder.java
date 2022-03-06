c9b550aee6932a8589553715efa3f57ce37259a3
harshitha <harshithauk1500@gmail.com>
2022-03-06 13:20:11 +0530

    made small modifications in code

--- src/main/java/powerpackage/powerfinder.java
+++ src/main/java/powerpackage/powerfinder.java
@@ -1,11 +1,12 @@
 package powerpackage;
-
-public class powerfinder {
-    public static int OF(int xyz, int pqr) {
-        int p = 1;
-        for (int i = 0; i<pqr; i++) {
-            p *= xyz;
-        }
+import Math;
+class powerfinder {
+    public static int OF(int xyz, int limit) {
+//        int p = 1;
+//        for (int i = 0; i<limit; i++) {
+//            p *= xyz;
+//        }
+        int p = Math.pow(xyz,limit);
         return p;
     }
 }
